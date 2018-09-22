68f0229e5725dc1902100c082e8d85b637b91bae
realsst1 <shreyesh2008@gmail.com>
2018-09-22 15:31:46 +0530

    notice board added

--- app/src/main/java/com/example/shreyesh/sarinstituteofmedicalscience/AdminHomeActivity.java
+++ app/src/main/java/com/example/shreyesh/sarinstituteofmedicalscience/AdminHomeActivity.java
@@ -9,12 +9,16 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -31,16 +35,16 @@ public class AdminHomeActivity extends AppCompatActivity implements NavigationVi
     private ActionBar actionBar;
     private NavigationView navigationView;
     private TextView inpatientCount, outpatientCount, doctorCount;
-    private DatabaseReference ipref, oref;
+    private DatabaseReference ipref, oref, noticeRef;
     private RecyclerView adminNoticeList;
-    private FloatingActionButton addNoticeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home);
 
-        addNoticeButton = (FloatingActionButton) findViewById(R.id.addNoticeButton);
+
         adminToolbar = (Toolbar) findViewById(R.id.adminHomeToolbar);
         setSupportActionBar(adminToolbar);
         actionBar = getSupportActionBar();
@@ -55,11 +59,19 @@ public class AdminHomeActivity extends AppCompatActivity implements NavigationVi
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setTitle("Admin Home");
 
+        adminNoticeList = (RecyclerView) findViewById(R.id.noticeAdminList);
+        adminNoticeList.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
         inpatientCount = (TextView) findViewById(R.id.inpatientCount);
         outpatientCount = (TextView) findViewById(R.id.outpatientCount);
         doctorCount = (TextView) findViewById(R.id.doctorCount);
         ipref = FirebaseDatabase.getInstance().getReference().child("patients").child("inpatients");
         oref = FirebaseDatabase.getInstance().getReference().child("patients").child("outpatients");
+        noticeRef = FirebaseDatabase.getInstance().getReference().child("notices");
+
+        ipref.keepSynced(true);
+        oref.keepSynced(true);
+        noticeRef.keepSynced(true);
 
         ipref.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -85,15 +97,33 @@ public class AdminHomeActivity extends AppCompatActivity implements NavigationVi
             }
         });
 
-        addNoticeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(AdminHomeActivity.this, AddNoticeActivity.class));
-            }
-        });
 
     }
 
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerAdapter<Notice, NoticeViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Notice, NoticeViewHolder>(
+                Notice.class,
+                R.layout.notice_single,
+                NoticeViewHolder.class,
+                noticeRef
+        ) {
+            @Override
+            protected void populateViewHolder(NoticeViewHolder viewHolder, Notice model, int position) {
+
+                viewHolder.setNoticeDate(model.getDate());
+                viewHolder.setNoticeTitle(model.getTitle());
+
+            }
+        };
+
+        adminNoticeList.setAdapter(firebaseRecyclerAdapter);
+        firebaseRecyclerAdapter.notifyDataSetChanged();
+        firebaseRecyclerAdapter.startListening();
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -101,7 +131,16 @@ public class AdminHomeActivity extends AppCompatActivity implements NavigationVi
         if (drawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
+        if (item.getItemId() == R.id.addNoticeButton) {
+            startActivity(new Intent(AdminHomeActivity.this, AddNoticeActivity.class));
+        }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.admin_home_toolbar, menu);
+        return true;
     }
 
     @Override
@@ -114,5 +153,27 @@ public class AdminHomeActivity extends AppCompatActivity implements NavigationVi
 
         }
         return true;
+    }
+
+    public static class NoticeViewHolder extends RecyclerView.ViewHolder {
+
+        View mView;
+
+        public NoticeViewHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+
+        public void setNoticeTitle(String title) {
+            TextView ntitle = (TextView) mView.findViewById(R.id.noticeSingleTitle);
+            ntitle.setText(title);
+        }
+
+        public void setNoticeDate(String date) {
+            TextView ndate = (TextView) mView.findViewById(R.id.noticeSingleDate);
+            ndate.setText(date);
+        }
+
+
     }
 }
